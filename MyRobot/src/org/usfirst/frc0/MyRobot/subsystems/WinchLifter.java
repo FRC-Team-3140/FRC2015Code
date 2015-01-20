78bf0f77023ee78301e109ae6d8b231bc40ad0f3
RccCookie <rcook184@gmail.com>
2015-01-20 18:35:54 -0500

    Added winch functionality and stuff.

--- MyRobot/src/org/usfirst/frc0/MyRobot/subsystems/WinchLifter.java
+++ MyRobot/src/org/usfirst/frc0/MyRobot/subsystems/WinchLifter.java
@@ -3,25 +3,23 @@ package org.usfirst.frc0.MyRobot.subsystems;
 import org.usfirst.frc0.MyRobot.RobotMap;
 import org.usfirst.frc0.MyRobot.commands.Lift;
 
-import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.Relay.Direction;
-import edu.wpi.first.wpilibj.Relay.Value;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
 public class WinchLifter extends Subsystem implements ILifter{
-	private Relay winchMotor = RobotMap.winchMotor;
+	private SpeedController winchMotor = RobotMap.winchMotor;
+	
 	public void liftUp() {
-		winchMotor.setDirection(Direction.kForward);
+		winchMotor.set(0.5); 
 	}
 	public void liftDown() {
-		winchMotor.setDirection(Direction.kReverse);
+		winchMotor.set(-0.5);
 	}
 	public void liftStop() {
-		winchMotor.set(Value.kOff);
+		winchMotor.set(0);
 	}
 	
 	
