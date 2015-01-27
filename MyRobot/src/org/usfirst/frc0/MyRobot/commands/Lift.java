a6abfc81f8ac9bd417f1b504b4a9609f227c9119
Kyle Parsley <parsley.kyle@gmail.com>
2015-01-26 20:58:16 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	MyRobot/src/org/usfirst/frc0/MyRobot/OI.java
    	MyRobot/src/org/usfirst/frc0/MyRobot/RobotMap.java
    	MyRobot/sysProps.xml

--- MyRobot/src/org/usfirst/frc0/MyRobot/commands/Lift.java
+++ MyRobot/src/org/usfirst/frc0/MyRobot/commands/Lift.java
@@ -28,7 +28,7 @@ public class Lift extends CommandBase {
 	
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		lift(oi.getLiftSpeed());
+		lift(getSpeed());
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
@@ -43,5 +43,21 @@ public class Lift extends CommandBase {
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+	}
+
+	private double getSpeed() {
+		// this does not use joystick tolerance since the speed control
+		// on the joysticks are precise
+		//double speed = (oi.getLiftDownButton()?-1:0) + (oi.getLiftUpButton()?1:0);
+		double speed = 0;
+		if (oi.getLiftUpButton() && !oi.getLiftDownButton()) {
+			speed = 1;
+		}
+		else if(oi.getLiftDownButton()) {
+			speed = -1;
+		}
+		
+		return oi.getLiftThrottle() * speed;
+			
 	}
 }
